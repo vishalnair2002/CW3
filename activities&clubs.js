3c2b9eb44ea4e44afd9f16e08f800f463928fd1d
Vishal Nair <148544787+vishalnair2002@users.noreply.github.com>
2024-02-27 11:30:53 +0400

    Add files via upload

---
+++ activities&clubs.js
@@ -0,0 +1,101 @@
+
+let activitiesAndClubsData = [
+    {
+        rating: 3,
+        id: 1001,
+        title: "Math",
+        room: "Classroom A",
+        location: "Jumeirah",
+        price: 100,
+        image: "images/Math.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1002,
+        title: "Science",
+        room: "Lab B",
+        location: "Al Qouz",
+        price: 120,
+        image: "images/Science.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1003,
+        title: "English",
+        room: "Room C",
+        location: "JVC",
+        price: 140,
+        image: "images/English.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1004,
+        title: "Music",
+        room: "Music Room",
+        location: "Marina",
+        price: 125,
+        image: "images/Music.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1005,
+        title: "Football",
+        location: "AL Wasl",
+        price: 160,
+        image: "images/Football.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1006,
+        title: "Cricket",
+        location: "Al Nasr",
+        price: 160,
+        image: "images/Cricket.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1007,
+        title: "Chess",
+        room: "Chess Club Room",
+        location: "Greens",
+        price: 145,
+        image: "images/Chess.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1008,
+        title: "Photography",
+        room: "Art Studio",
+        location: "Karama",
+        price: 150,
+        image: "images/Photography.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1009,
+        title: "Arts",
+        room: "Art Room",
+        location: "Silicon",
+        price: 110,
+        image: "images/Arts.jpg",
+        availableInventory: 5
+    },
+    {
+        rating: 3,
+        id: 1010,
+        title: "Yoga",
+        room: "Yoga Studio",
+        location: "Falcon",
+        price: 130,
+        image: "images/Yoga.jpg",
+        availableInventory: 5
+    }
+]
